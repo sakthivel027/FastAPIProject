54d25e61db5bcea7e53d5b423f3ea671fa7a075c
Sakthivel <73012840+sakthivel027@users.noreply.github.com>
2025-01-31 19:42:10 +0530

    Create main.py
    added main

---
+++ main.py
@@ -0,0 +1,35 @@
+main.py
+#a=5
+#c=lambda a:a+10
+#print(c(5))
+
+#str
+#[::-1]
+#str=[1,2,3,4,]
+#str =  "1234321" #MADAM
+#print(str[::-1])
+#a = reverse(str)
+#str.join('str')
+#print(str)
+#if str==str[::-1]:
+#    print(str)
+#else:
+#    print('re')
+#    pass
+
+### Program to check if a string is palindrome or not
+##
+##my_str = 'aIbohPhoBiA'
+##
+### make it suitable for caseless comparison
+##my_str = my_str.casefold()
+##
+### reverse the string
+##rev_str = reversed(my_str)
+##
+### check if the string is equal to its reverse
+##if list(my_str) == list(rev_str):
+##   print("The string is a palindrome.")
+##else:
+##   print("The string is not a palindrome.")
+
